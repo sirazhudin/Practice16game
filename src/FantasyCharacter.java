75d0bb620178d7b4fe91d3cdf37ec34b89ac54f4
sss <sirazhudin@mail.ru>
2022-08-03 20:47:18 -0400

    test

---
+++ src/FantasyCharacter.java
@@ -0,0 +1,57 @@
+public abstract class FantasyCharacter implements Fighter {
+    private String name;
+    private int healthPoints;
+    private int strength;
+    private int dexterity;
+
+    private int xp;
+    private int gold;
+
+    public FantasyCharacter(String name, int healthPoints, int strength,
+                            int dexterity, int xp, int gold) {
+        this.name=name;
+        this.healthPoints=healthPoints;
+        this.strength=strength;
+        this.dexterity=dexterity;
+        this.xp=xp;
+        this.gold=gold;
+    }
+    @Override
+    public int attack() {
+        if(dexterity * 3 > getRandomValue()) return strength;
+        else return 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    private int getRandomValue() {
+        return (int)(Math.random() * 100);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s здоровье:%d",name,healthPoints);
+    }
+}
